f2552995d6b0dc390eef28b85d7dea24a28f698a
Michael Forson <micfor722@gmail.com>
2024-08-13 00:02:51 +0000

    forson commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import MainLayout from "./layout/Mainlayout";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route path="/" element={<MainLayout />}></Route>)
+);
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default App;
---
+++ src/layout/Mainlayout.jsx
@@ -0,0 +1,10 @@
+import { Outlet } from "react-router-dom";
+const MainLayout = () => {
+  return (
+    <>
+      <div>Hello world</div>
+    </>
+  );
+};
+
+export default MainLayout;
